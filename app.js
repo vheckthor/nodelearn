bf14c46779ac4f02050b29ca0b2b0143c14c2669
victor Adebayo <adebayovicktor@gmail.com>
2021-09-22 23:38:14 +0100

    added cors for development

--- app.js
+++ app.js
@@ -26,6 +26,13 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}))
 app.use(require('body-parser').json());
 
+// enable CORS without external module
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.get('/', (req, res) => {
 
     res.redirect('/blogs');
